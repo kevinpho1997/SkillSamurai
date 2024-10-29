31647e54419db1247763b9afac18ed8fe941b6ef
Kevin Pho <88666647+kevinpho1997@users.noreply.github.com>
2024-10-29 23:51:19 +1000

    alphabet challenge

--- Testing/alphabet.py
+++ Testing/alphabet.py
@@ -1,2 +1,13 @@
-alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+import random as rd
 
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+tempWord = ""
+
+pwLength = 6
+
+for i in range(pwLength):
+    randomIndex = rd.randint(0, len(alphabet))
+    tempWord = tempWord + alphabet[randomIndex]
+
+finalWord = tempWord
+print(finalWord)
